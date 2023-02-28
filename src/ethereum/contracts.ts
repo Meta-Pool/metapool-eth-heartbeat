7baff4f8604bb291d4c6e10fe645d16623a97e33
Calcifer1001 <dseidlertoken@gmail.com>
2023-02-28 15:48:55 -0300

    [IMPR] assign bot from bash to ts

--- src/ethereum/contracts.ts
+++ src/ethereum/contracts.ts
@@ -1,24 +1,12 @@
 import { Contract, ethers } from "ethers";
-import dotenv from "dotenv"
-import path from "path"
+import { ENV, getConfig } from "../entities/env";
 
 const NETWORK = 'goerli'
 const RPC_URL = "https://goerli.infura.io/v3/"
 // TODO regenerate private data and get from .env
 const API_KEY = "mrTmFCjo_n7xJBq-V3Oli5AuQiqH3GEy"
-const ACCOUNT_PRIVATE_KEY = "0x97ea350f26d4f33264db4b01c2f285ddfd4ba36cb188341d9b8027da3033ba0d"
 
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
-interface ENV {
-    ACCOUNT_PRIVATE_KEY: string | undefined;
-}
-
-function getConfig(): ENV {
-    return {
-        ACCOUNT_PRIVATE_KEY: process.env.ACCOUNT_PRIVATE_KEY,
-    };
-  };
 
 function getProvider(network: string, apiKey: string) {
     return new ethers.AlchemyProvider(
