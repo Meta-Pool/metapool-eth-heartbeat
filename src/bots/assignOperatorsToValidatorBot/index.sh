2fc0b4bee54a43ab5ae2f3734b7b7f6cbc917dd1
Calcifer1001 <dseidlertoken@gmail.com>
2023-02-28 10:04:33 -0300

    [FIX] assign bot fixes

--- src/bots/assignOperatorsToValidatorBot/index.sh
+++ src/bots/assignOperatorsToValidatorBot/index.sh
@@ -25,39 +25,24 @@ IFS="|"
 # OPERATOR_KEYS_STRINGS=$(jq --argjson OPERATOR_IDS_ARRAY "$OPERATOR_IDS_ARRAY" '.[] | select( .id == $OPERATOR_IDS_ARRAY[0] or .id == $OPERATOR_IDS_ARRAY[1] or .id == $OPERATOR_IDS_ARRAY[2] or .id == $OPERATOR_IDS_ARRAY[3]) | .pubkey' src/bots/assignOperatorsToValidatorBot/operators.json)
 OPERATOR_KEYS_STRINGS=$(jq --arg IDS "${OPERATOR_IDS[*]}" '($IDS | split(",") | map(tonumber)) as $PIDS | .[] | select( [.id] | index($PIDS[])).pubkey' $ALL_JSONS_PATH/operators.json)
 OPERATOR_KEYS=$(echo $OPERATOR_KEYS_STRINGS | /usr/bin/sed -z 's/\n/,/g;s/,$/\n/;s/"//g;s/ /,/g')
-echo $OPERATOR_KEYS
 
 # Run command below
-# ssv-keys-lin ksh --ksv=3 --keystore=$KEYSTORE_PATH --password=$PASSWD --operator-ids=$OPERATOR_IDS --operator-keys=$OPERATOR_KEYS --ssv-token-amount=0 --output-folder=./src/validatorsData/keyshares
+$ALL_JSONS_PATH/ssv-keys-lin ksh --ksv=3 --keystore=$KEYSTORE_PATH --password=$PASSWD --operator-ids=$OPERATOR_IDS --operator-keys=$OPERATOR_KEYS --ssv-token-amount=0 --output-folder=./src/validatorsData/keyshares
 
 # Update assigned.json adding validator pubkey from keystore
-# PUB_KEY=$(jq '.pubkey' $KEYSTORE_PATH | /usr/bin/sed -z 's/"//g')
-# ASSIGNED_GROUPS=$(/usr/bin/jq '. | length' $ALL_JSONS_PATH/assigned.json)
-# if [[ $ASSIGNED_GROUPS -eq $NEXT_GROUP_INDEX ]]; then
-#     # Creates new group with no validators assigned, ready to assign one on next step
-#     echo $(/usr/bin/jq --arg OPERATOR_IDS "$OPERATOR_IDS" --arg ALL_JSONS_PATH "$ALL_JSONS_PATH" '.[.|length] |= . + {"ids":[$OPERATOR_IDS], "pubkeys":[]}' $ALL_JSONS_PATH/assigned.json) > $ALL_JSONS_PATH/assigned.json
-# fi
-# echo $(/usr/bin/jq --arg PUB_KEY "$PUB_KEY" --arg ALL_JSONS_PATH "$ALL_JSONS_PATH" '.[length-1].pubkeys |= . + [$PUB_KEY]' $ALL_JSONS_PATH/assigned.json) > $ALL_JSONS_PATH/assigned.json
+PUB_KEY=$(jq '.pubkey' $KEYSTORE_PATH | /usr/bin/sed -z 's/"//g')
+ASSIGNED_GROUPS=$(/usr/bin/jq '. | length' $ALL_JSONS_PATH/assigned.json)
+if [[ $ASSIGNED_GROUPS -eq $NEXT_GROUP_INDEX ]]; then
+    # Creates new group with no validators assigned, ready to assign one on next step
+    echo $(/usr/bin/jq --arg OPERATOR_IDS "$OPERATOR_IDS" --arg ALL_JSONS_PATH "$ALL_JSONS_PATH" '.[.|length] |= . + {"ids":[$OPERATOR_IDS], "pubkeys":[]}' $ALL_JSONS_PATH/assigned.json) > $ALL_JSONS_PATH/assigned.json
+fi
+echo $(/usr/bin/jq --arg PUB_KEY "$PUB_KEY" --arg ALL_JSONS_PATH "$ALL_JSONS_PATH" '.[length-1].pubkeys |= . + [$PUB_KEY]' $ALL_JSONS_PATH/assigned.json) > $ALL_JSONS_PATH/assigned.json
 
 # Send the keyshare to the ssv contract via js
-# node dist/bots/assignOperatorsToValidatorBot/index.js $pubkey$ $OPERATOR_IDS
-
-
-
-
-
-
-# We will take the operator ids from a groups.json file. First we'll check how many validators have operators assignated depending on assigned.json
-# s/\n/,/g;s/,$/\n/ is used to replace the character "new_line" with ",". For some reason, using a variable doesn't produce the desired output
-#OPERATOR_IDS=$(/usr/bin/jq '.[].id' $PATH | /usr/bin/sed -z 's/\n/,/g;s/,$/\n/')
-# s/\n/,/g;s/,$/\n/;s/"//g is the same as above, but adds replacing "\"" for nothing
-#OPERATOR_KEYS=$(/usr/bin/jq '.[].pubkey' $PATH | /usr/bin/sed -z 's/\n/,/g;s/,$/\n/;s/"//g')
-
-# echo $ASSIGNED_VALIDATORS
-# echo $NEXT_GROUP_INDEX
-# echo $OPERATOR_IDS
-# echo $OPERATOR_KEYS
-# echo $PUB_KEY
-
-
-#node index.js
+KEYSHARE_PATH=/home/calcifer/ssv-keys/keyshares-20230213_043527.json
+# TODO SELECT PROPER PATH FOR KEYSHARE
+KEY_SHARE_PUB_KEY=$(jq '.data.publicKey' $KEYSHARE_PATH)
+SHARES_PUB_KEYS=$(jq '.data.shares.publicKey' $KEYSHARE_PATH)
+SHARES_ENCRYPTED=$(jq '.data.shares.encryptedKeys' $KEYSHARE_PATH)
+AMOUNT=$(jq '.data.publicKey' $KEYSHARE_PATH)
+node dist/bots/assignOperatorsToValidatorBot/index.js $KEY_SHARE_PUB_KEY $OPERATOR_IDS $SHARES_PUB_KEYS $SHARES_ENCRYPTED $AMOUNT
--- src/bots/assignOperatorsToValidatorBot/index.ts
+++ src/bots/assignOperatorsToValidatorBot/index.ts
@@ -1,16 +1,17 @@
 import { registerValidator } from "../../ethereum/ssv"
-import { Command } from 'commander'
 
 // Call the corresponding ssv function to confirm the operators, sending the keyshare file
 async function run() {
     // if(process.argv.length != 7) throw new Error("Be sure to send all the parameters")
     let pubkey = process.argv[2]
     let operatorIds = process.argv[3].split(",").map(Number)
-    let sharesPubKeys  = process.argv[4]
-    let sharesEncrypted  = process.argv[5]
-    let amount  = process.argv[6]
+    let sharesPubKeys: string[]  = process.argv[4].split(",")
+    let sharesEncrypted: string[]  = process.argv[5].split(",")
+    let amount: number = Number(process.argv[6])
+
+    await registerValidator(pubkey, operatorIds, sharesPubKeys, sharesEncrypted, amount)
     
-    console.log(operatorIds)
+    
     // registerValidator()
 
 }
