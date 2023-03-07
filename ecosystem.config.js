a62cebf3d365c1aab01a08f081ee67f6c8820dcf
Calcifer1001 <dseidlertoken@gmail.com>
2023-03-07 14:48:14 -0300

    [ADD] ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'ssv-heartbeat',
+    cwd: 'dist',
+    script: 'index.js',
+    restart_delay: 1000,
+    watch: 'index.js',
+    out_file: 'main.log',
+    error_file: 'main.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
+  }]
+};
+
