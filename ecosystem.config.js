fd32b6fb74679348f8684d2012e7b2c2d67c7061
Calcifer1001 <dseidlertoken@gmail.com>
2023-03-07 15:10:56 -0300

    [FIX] index location

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps : [{
     name: 'ssv-heartbeat',
     cwd: 'dist',
-    script: 'index.js',
+    script: 'bots/heartbeat/index.js',
     restart_delay: 1000,
-    watch: 'index.js',
+    watch: 'bots/heartbeat/index.js',
     out_file: 'main.log',
     error_file: 'main.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss:SSS',
