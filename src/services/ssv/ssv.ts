0a1d3cf4b621e1c75371c0135c5ec60b4d1ff9fe
Calcifer1001 <dseidlertoken@gmail.com>
2023-06-06 17:06:03 -0300

    [ADD] ssv files

---
+++ src/services/ssv/ssv.ts
@@ -0,0 +1,3 @@
+const validatorsDataUrl = "https://api.ssv.network/api/v3/prater/search?search=943f801faae1e38e56148ad87580b775be27089c564803fd5d182a2e8ba1c4781856e5c7f9b3b81911becd64ed735813&searchFor=both&operatorsLimit=5&validatorsLimit=5"
+const operatorData =  "https://api.ssv.network/api/v3/prater/operators/85"
+
