dde6e2f335f906b9f8c427141f0b343f2b5d02c3
Calcifer1001 <dseidlertoken@gmail.com>
2024-07-10 11:18:07 -0300

    feat: add depositors length validation

--- src/bots/moveAuroraDelayedUnstakeOrders/index.ts
+++ src/bots/moveAuroraDelayedUnstakeOrders/index.ts
@@ -10,12 +10,14 @@ import { isTestnet } from "../heartbeat"
 export async function checkAuroraDelayedUnstakeOrders(useOldContract: boolean): Promise<boolean> {
     try {
         const stakingManagerContract = new StakingManagerContract(useOldContract)
+        const depositorsLength: bigint = await stakingManagerContract.getDepositorsLength()
         const nextRunTimestampInSeconds: BigInt = await stakingManagerContract.nextCleanOrderQueue()
         const nextRunTimestampInMs: number = Number(nextRunTimestampInSeconds.toString()) * 1000
         const now = new Date().getTime()
+        console.log("Depositors length", depositorsLength)
         console.log("Next clean orders queue", new Date(nextRunTimestampInMs))
         console.log("Time remaining to next run", sLeftToTimeLeft(Math.floor((nextRunTimestampInMs - now) / 1000)))
-        if(now >= nextRunTimestampInMs) {
+        if(now >= nextRunTimestampInMs && depositorsLength > 0n) {
             console.log("Running clean orders queue")
             // const tx = await stakingManagerContract.cleanOrdersQueue()
             const tx = await stakingManagerContract.cleanOrdersQueue()
